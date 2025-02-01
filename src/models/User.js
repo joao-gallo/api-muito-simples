dae809b9955164cf1101e93bf6cdd0abc6debb25
joao-gallo <joaogallop@gmail.com>
2025-02-01 16:04:37 -0300

    ä

--- src/models/User.js
+++ src/models/User.js
@@ -7,7 +7,7 @@ const User = sequelize.define("User", {
         autoIncrement: true,
         primaryKey: true,
     },
-    nome: {
+    name: {
         type: DataTypes.STRING(100),
         allowNull: false,
     },
--- src/routes/users.js
+++ src/routes/users.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 
 router.get("/", userController.getUsers);
-router.post("/", userController.createUser);
+router.post("/teste", userController.createUser);
 
 module.exports = router;
