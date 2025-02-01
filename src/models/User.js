994da944e2b43160871ffc893fb3b55b48e92d7e
joao-gallo <joaogallop@gmail.com>
2025-02-01 15:34:31 -0300

    model e sequelizr

---
+++ src/models/User.js
@@ -0,0 +1,32 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/sequelize");
+
+const User = sequelize.define("User", {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    nome: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        unique: true,
+    },
+    senha: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+    },
+    created_at: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+    },
+}, {
+    tableName: "users",
+    timestamps: false
+});
+
+module.exports = User;
